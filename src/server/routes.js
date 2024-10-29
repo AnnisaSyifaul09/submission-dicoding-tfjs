fd1436bb1014ad88bafb531e22480f6c9f7244f1
AnnisaSyifaul09 <annisasyifaul78@gmail.com>
2024-10-29 12:40:38 +0700

    update

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,4 +1,5 @@
 const postPredictHandler = require('../server/handler');
+const { Firestore } = require('@google-cloud/firestore'); // Ensure proper import
 
 const routes = [
   {
@@ -7,19 +8,49 @@ const routes = [
     handler: postPredictHandler,
     options: {
       payload: {
+        maxBytes: 1000000,
         allow: 'multipart/form-data',
-        multipart: true
-      }
-    }
+        multipart: true,
+      },
+    },
   },
   {
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
+    method: "GET",
+    path: "/predict/histories",
+    handler: async () => {
+      try {
+        const db = new Firestore({
+          projectId: 'submissionmlgc-annisasyifaul',
+        });
+        const querySnapshot = await db.collection("predictions").get();
+        const data = [];
 
-        return 'Hello World!';
-    }
-}
-]
+        querySnapshot.forEach((doc) => {
+          const docData = doc.data();
+          const formattedData = {
+            id: doc.id,
+            history: {
+              result: docData.result,
+              createdAt: docData.createdAt,
+              suggestion: docData.suggestion,
+            },
+          };
+          data.push(formattedData);
+        });
+
+        return {
+          status: "success",
+          data: data,
+        };
+      } catch (error) {
+        console.error("Error fetching prediction histories:", error);
+        return {
+          status: "fail",
+          message: "An internal server error occurred",
+        };
+      }
+    },
+  },
+];
 
 module.exports = routes;
